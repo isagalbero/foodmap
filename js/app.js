03921c464a0bee0a7e2729e3bdb8f9e40af270d6
isagalbero <isagalbero@gmail.com>
2018-08-06 10:14:42 -0300

    Still working on it

--- js/app.js
+++ js/app.js
@@ -1,1 +1,48 @@
 // Add your JavaScript
+
+$(document).ready(function(){
+
+  $('.splash').fadeOut(2000, function(){
+    $('.splash').addClass('home-display');
+    $('section').removeClass('container');
+  });
+
+
+
+
+
+ 
+
+
+});
+
+var map;
+function initMap() {
+map = new google.maps.Map(document.getElementById('map'), {
+zoom: 14,
+center: new google.maps.LatLng(-23.5559376,-46.6574013),
+});
+};
+
+var x;
+var y;
+
+function markers () {
+  new google.maps.Marker({
+    position: new google.maps.LatLng(x, y),
+    map: map
+  });
+};
+
+function allMarkers () {
+  for (var i=0; i < restaurantes.length; i++) {
+    x = restaurantes[i]['latitude'];
+    y = restaurantes[i]['longitude'];
+    markers();
+  }
+};
+
+window.onload = function(){
+  initMap();
+  allMarkers();
+};
